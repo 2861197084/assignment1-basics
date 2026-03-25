c6864a61041ec66568c0090713ab6fc70fcf6e54
2861197084 <2861197084@qq.com>
2026-03-25 20:57:42 +0800

    Implement Embedding class and update run_embedding function for integration

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -31,3 +31,27 @@ class Linear(nn.Module):
         return torch.einsum('...i,oi->...o', x, self.weight) + (
             self.bias if self.bias is not None else 0
         )
+
+
+class Embedding(nn.Module):
+    num_embeddings: int
+    embedding_dim: int
+    weight: torch.Tensor
+
+    def __init__(
+        self,
+        num_embeddings: int,
+        embedding_dim: int,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None
+    ) -> None:
+        kwargs = {'device': device, 'dtype': dtype}
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.weight = nn.Parameter(
+            torch.empty((num_embeddings,embedding_dim), **kwargs)
+        )
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        return self.weight[token_ids]
