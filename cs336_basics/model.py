1961154db821e1c22340b7afe304421e598ce759
2861197084 <2861197084@qq.com>
2026-03-26 22:36:39 +0800

    Implement RMSNorm and SwiGLU classes, and update run_swiglu and run_rmsnorm functions for integration

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -55,3 +55,53 @@ class Embedding(nn.Module):
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.weight[token_ids]
+
+
+class RMSNorm(nn.Module):
+    d_model: int
+    eps: float
+    weight: torch.Tensor
+
+    def __init__(
+        self,
+        d_model: int,
+        eps: float = 1e-5,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None
+    ) -> None:
+        kwargs = {'device': device, 'dtype': dtype}
+        super().__init__()
+        self.weight = nn.Parameter(torch.empty(d_model, **kwargs))
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        var = x.pow(2).mean(dim=-1, keepdim=True) + self.eps
+        x_out = x * var.rsqrt() * self.weight
+        return x_out.to(in_dtype)
+
+def norm(x:torch.Tensor, eps:float=1e-6) -> torch.Tensor:
+    """Functional RMS normalization without learnable parameters"""
+    var = x.pow(2).mean(dim=-1, keepdim=True) + eps
+    return x * var.rsqrt()
+
+def silu(x: torch.Tensor) -> torch.Tensor:
+    return x * torch.sigmoid(x)
+
+class SwiGLU(nn.Module):
+    d_model: int
+    d_ff: int
+
+    def __init__(self, d_model: int, d_ff: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.w1 = Linear(in_features=d_model, out_features=d_ff)
+        self.w2 = Linear(in_features=d_ff, out_features=d_model)
+        self.w3 = Linear(in_features=d_model, out_features=d_ff)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.w2(silu(self.w1(x)) * self.w3(x))
+    
+
